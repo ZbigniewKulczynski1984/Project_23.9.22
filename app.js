938ba1654eef4ed7e04842610588e08f5d7e0366
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-23 11:13:49 +0200

    challenge done

--- app.js
+++ app.js
@@ -7,30 +7,43 @@
 // const test3 = document.getElementsByClassName('test')
 // console.log(test3)
 
-const test = document.querySelector('#item')
-console.log(test)
+// const test = document.querySelector('#item')
+// console.log(test)
 
-const test2 = document.querySelectorAll('li')
-console.log(test2)
+// const test2 = document.querySelectorAll('li')
+// console.log(test2)
 
-const ulList = document.querySelector('ul')
-console.log(ulList)
+// const ulList = document.querySelector('ul')
+// console.log(ulList)
 
-const liItem = ulList.querySelector('li')
-console.log(liItem)
+// const liItem = ulList.querySelector('li')
+// console.log(liItem)
 
-//żywe kolekcje
-const newLi = document.createElement('li')
-ulList.appendChild(newLi).textContent = '👀'
+// //żywe kolekcje
+// const newLi = document.createElement('li')
+// ulList.appendChild(newLi).textContent = '👀'
 
-const heading = document.getElementsByTagName('h2')
-console.log(heading)
+// const heading = document.getElementsByTagName('h2')
+// console.log(heading)
 
-const paragraphs = document.querySelectorAll('p')
-console.log(paragraphs)
+// const paragraphs = document.querySelectorAll('p')
+// console.log(paragraphs)
 
-const testDiv = document.querySelector('.test')
-console.log(testDiv)
+// const testDiv = document.querySelector('.test')
+// console.log(testDiv)
 
-const paragraph2 = testDiv.querySelector('#test')
-console.log(paragraph)
+// const paragraph2 = testDiv.querySelector('#test')
+// console.log(paragraph)
+
+// const ulList = document.createElement('ul')
+// const liItem = document.createElement('li')
+// liItem.textContent = 'Cześć'
+
+// document.body.appendChild(ulList)
+// ulList.appendChild(liItem)
+
+const testDiv = document.querySelector('div')
+
+const paragraph = document.createElement('p')
+testDiv.appendChild(paragraph)
+paragraph.textContent = 'Lorem ipsum'
