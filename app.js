718ae3c0c8cd8ffaf49a38720f1f343016f2196b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-23 10:34:10 +0200

    elements gotted

---
+++ app.js
@@ -0,0 +1,8 @@
+const test = document.getElementById('item');
+console.log(test);
+
+const test2 = document.getElementsByTagName('ul')
+console.log(test2)
+
+const test3 = document.getElementsByClassName('test')
+console.log(test3)
